5a62b35be5aff70ab8c4113ef4dcb5d7f45c49a7
Victor Azevedo <josevcazevedo@gmail.com>
2023-05-29 10:37:00 -0300

    refactor: SkillsSections

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,21 +1,18 @@
+import { SkillsList, skillsListIcons } from "../../mock/skillsList";
+
 interface ISkill {
-  children: React.ReactNode;
-  name: string;
-  description?: string;
+  name: SkillsList;
 }
 
-export const Skill: React.FC<ISkill> = ({ children, name, description }) => {
+export const Skill: React.FC<ISkill> = ({ name }) => {
   return (
     <div className="flex flex-col justify-center items-center gap-4">
       <div className="w-14 h-14 bg-zinc-300 rounded-2xl flex items-center justify-center ">
-        {children}
+        {skillsListIcons[name]}
       </div>
       <h5 className="font-Jakarta text-lg font-medium text-center min-w-[110px]">
         {name}
       </h5>
-      {description && (
-        <p className="text-center min-w-[350px]">{description}</p>
-      )}
     </div>
   );
 };
--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,18 +1,4 @@
-import { FaNodeJs } from "react-icons/fa";
-import {
-  SiCypress,
-  SiJavascript,
-  SiJest,
-  SiMongodb,
-  SiNestjs,
-  SiNextdotjs,
-  SiPostgresql,
-  SiPrisma,
-  SiReact,
-  SiTailwindcss,
-  SiTypescript,
-} from "react-icons/si";
-
+import { SkillsList, skillsList } from "../../mock/skillsList";
 import { AboveTitle } from "./AboveTitle";
 import { SectionTitle } from "./SectionTitle";
 import { Skill } from "./Skill";
@@ -25,42 +11,9 @@ export const SkillsSection = () => {
         <SectionTitle title="Especializado em" />
       </div>
       <div className="flex justify-center items-center gap-10 flex-wrap">
-        <Skill name="Javascript">
-          <SiJavascript className="text-3xl fill-indigo-500" />
-        </Skill>
-        <Skill name="Typescript">
-          <SiTypescript className="text-3xl fill-indigo-500" />
-        </Skill>
-        <Skill name="React">
-          <SiReact className="text-3xl fill-indigo-500" />
-        </Skill>
-        <Skill name="NextJs">
-          <SiNextdotjs className="text-3xl fill-indigo-500" />
-        </Skill>
-        <Skill name="Tailwind">
-          <SiTailwindcss className="text-3xl fill-indigo-500" />
-        </Skill>
-        <Skill name="NodeJs">
-          <FaNodeJs className="text-3xl fill-indigo-500" />
-        </Skill>
-        <Skill name="NestJs">
-          <SiNestjs className="text-3xl fill-indigo-500" />
-        </Skill>
-        <Skill name="Jest">
-          <SiJest className="text-3xl fill-indigo-500" />
-        </Skill>
-        <Skill name="Cypress">
-          <SiCypress className="text-3xl fill-indigo-500" />
-        </Skill>
-        <Skill name="PostgreSQL">
-          <SiPostgresql className="text-3xl fill-indigo-500" />
-        </Skill>
-        <Skill name="Prisma">
-          <SiPrisma className="text-3xl fill-indigo-500" />
-        </Skill>
-        <Skill name="MongoDB">
-          <SiMongodb className="text-3xl fill-indigo-500" />
-        </Skill>
+        {skillsList.map((skill: SkillsList) => (
+          <Skill key={skill} name={skill} />
+        ))}
       </div>
     </section>
   );
