1f32adcc339f357354aba27f525776a2eeeaf161
Victor Azevedo <josevcazevedo@gmail.com>
2023-05-29 17:18:09 -0300

    style: SocialNetwork start

--- src/components/SocialNetworks.tsx
+++ src/components/SocialNetworks.tsx
@@ -2,7 +2,7 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const SocialNetworks = () => {
   return (
-    <div className="inline-flex justify-center items-center gap-4 mt-8">
+    <div className="inline-flex justify-start items-center gap-4 mt-8">
       <a
         href="https://www.linkedin.com/in/victor-azevedo-dev/"
         target="_blank"
