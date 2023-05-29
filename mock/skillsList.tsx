da3116e56b13747b667c2a58f117af833f5c9e57
Victor Azevedo <josevcazevedo@gmail.com>
2023-05-29 17:25:09 -0300

    refactor: skills sort

--- mock/skillsList.tsx
+++ mock/skillsList.tsx
@@ -2,6 +2,7 @@ import { FaNodeJs } from "react-icons/fa";
 import {
   SiCypress,
   SiJavascript,
+  SiTypescript,
   SiJest,
   SiMongodb,
   SiNestjs,
@@ -16,17 +17,17 @@ export type Skills = keyof typeof skillsListIcons;
 
 export const skillsListIcons = {
   Javascript: <SiJavascript className="text-3xl fill-indigo-500" />,
-  Typescript: <SiJavascript className="text-3xl fill-indigo-500" />,
+  Typescript: <SiTypescript className="text-3xl fill-indigo-500" />,
   React: <SiReact className="text-3xl fill-indigo-500" />,
   NextJs: <SiNextdotjs className="text-3xl fill-indigo-500" />,
   Tailwind: <SiTailwindcss className="text-3xl fill-indigo-500" />,
   NodeJs: <FaNodeJs className="text-3xl fill-indigo-500" />,
   NestJs: <SiNestjs className="text-3xl fill-indigo-500" />,
+  Prisma: <SiPrisma className="text-3xl fill-indigo-500" />,
+  PostgreSQL: <SiPostgresql className="text-3xl fill-indigo-500" />,
+  MongoDB: <SiMongodb className="text-3xl fill-indigo-500" />,
   Jest: <SiJest className="text-3xl fill-indigo-500" />,
   Cypress: <SiCypress className="text-3xl fill-indigo-500" />,
-  PostgreSQL: <SiPostgresql className="text-3xl fill-indigo-500" />,
-  Prisma: <SiPrisma className="text-3xl fill-indigo-500" />,
-  MongoDB: <SiMongodb className="text-3xl fill-indigo-500" />,
 };
 
 export const skillsList = Object.keys(skillsListIcons) as unknown as Skills[];
