b3850bf66059f2880c529541c48f2ebc8dd6db38
Victor Azevedo <josevcazevedo@gmail.com>
2023-06-05 11:22:17 -0300

    chore: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import { SideBarProvider } from "@/contexts/SideBarContext";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <SideBarProvider>
         <Component {...pageProps} />
+        <Analytics />
       </SideBarProvider>
     </ThemeProvider>
   );
