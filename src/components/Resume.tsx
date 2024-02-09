01adb019063dad9cbbc7be8ba018123895b89993
Victor Azevedo <100978477+victor-azevedo@users.noreply.github.com>
2024-02-09 11:31:53 -0300

    Update Resume.tsx

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <a
       className="py-1 px-2 border-solid border-2 border-indigo-400 rounded-md text-indigo-400"
-      href="https://1drv.ms/b/s!AtrtC9vXbjBPi-06nfbBRKKYXbqwVQ?e=iQeyxW"
+      href="https://1drv.ms/b/s!AtrtC9vXbjBPjK0Sc9vOlZOSPV9frQ?e=N4IYsW"
       target="_blank"
     >
       Currículo
